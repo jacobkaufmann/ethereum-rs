6843e303f049b6f606a332db29e9edd91d58f0b0
jacobkaufmann <jacobkaufmann18@gmail.com>
2022-01-07 23:31:14 -0700

    Add enhancements to AccruedSubstate

--- src/core/vm/substate.rs
+++ src/core/vm/substate.rs
@@ -6,7 +6,7 @@ use crate::core::types::log::Log;
 /// Accrued transaction substate.
 ///
 /// Information acted upon immediately following a transaction.
-struct AccruedSubstate {
+pub struct AccruedSubstate {
     /// Self-destruct set. A set of accounts that will be discarded following the
     /// completion of the associated transaction.
     self_destruct_accounts: HashSet<Address>,
@@ -18,4 +18,23 @@ struct AccruedSubstate {
     touched_accounts: HashSet<Address>,
     /// Refund balance.
     refund_balance: U256,
+    /// Accessed account set.
+    accessed_accounts: HashSet<Address>,
+    /// Accessed storage set.
+    accessed_storage: HashSet<(Address, U256)>,
+}
+
+impl AccruedSubstate {
+    /// Returns an empty accrued transaction substate.
+    // TODO: Add precompile accounts to accessed account set.
+    pub fn empty() -> Self {
+        AccruedSubstate {
+            self_destruct_accounts: HashSet::new(),
+            log_series: Vec::new(),
+            touched_accounts: HashSet::new(),
+            refund_balance: U256::zero(),
+            accessed_accounts: HashSet::new(),
+            accessed_storage: HashSet::new(),
+        }
+    }
 }

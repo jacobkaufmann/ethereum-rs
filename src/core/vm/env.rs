452f099e91ea5acebc34ae6ab1c43516c91d0a45
jacobkaufmann <jacobkaufmann18@gmail.com>
2022-01-17 23:22:23 -0700

    Add enhancements to ExecutionEnv

--- src/core/vm/env.rs
+++ src/core/vm/env.rs
@@ -13,7 +13,7 @@ pub struct ExecutionEnv {
     data: Vec<u8>,
     /// Address of the account that caused the code to execute.
     sender: Address,
-    /// Value (in Wei) passed to `account` as part of the same procedure as execution.
+    /// Value (in Wei) passed to account as part of the same procedure as execution.
     value: U256,
     /// Machine code to be executed.
     machine_code: Vec<u8>,
@@ -24,4 +24,57 @@ pub struct ExecutionEnv {
     depth: U256,
     /// Permission to make modifications to the state.
     write_access: bool,
+}
+
+impl ExecutionEnv {
+    /// Returns the account that owns the code that is executing.
+    pub fn account(&self) -> &Address {
+        &self.account
+    }
+
+    /// Returns the sender address of the transaction that originated this execution.
+    pub fn origin(&self) -> &Address {
+        &self.origin
+    }
+
+    /// Returns the gas price in the transaction that originated this execution.
+    pub fn gas_price(&self) -> &U256 {
+        &self.gas_price
+    }
+
+    /// Returns the input data to this execution.
+    pub fn data(&self) -> &Vec<u8> {
+        &self.data
+    }
+
+    /// Returns the address of the account that caused the code to execute.
+    pub fn sender(&self) -> &Address {
+        &self.sender
+    }
+
+    /// Returns the value (in Wei) passed to account as part of the same procedure as execution.
+    pub fn value(&self) -> &U256 {
+        &self.value
+    }
+
+    /// Returns the machine code to be executed.
+    pub fn machine_code(&self) -> &Vec<u8> {
+        &self.machine_code
+    }
+
+    /// Returns the block header of the current block.
+    pub fn header(&self) -> &BlockHeader {
+        &self.header
+    }
+
+    /// Returns the depth of the present message call or contract creation. The number of CALLs or
+    /// CREATE(2)s being executed at present.
+    pub fn depth(&self) -> &U256 {
+        &self.depth
+    }
+
+    /// Returns the permission to make modifications to the state.
+    pub fn write_access(&self) -> bool {
+        self.write_access
+    }
 }

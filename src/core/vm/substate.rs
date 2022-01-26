39d64fb977031813c12b3889e660694308c8642f
jacobkaufmann <jacobkaufmann18@gmail.com>
2022-01-25 22:32:33 -0700

    Add enhancements to AccruedSubstate

--- src/core/vm/substate.rs
+++ src/core/vm/substate.rs
@@ -37,4 +37,34 @@ impl AccruedSubstate {
             accessed_storage: HashSet::new(),
         }
     }
+
+    /// Returns the self-destruct set.
+    pub fn self_destruct_accounts(&self) -> &HashSet<Address> {
+        &self.self_destruct_accounts
+    }
+
+    /// Returns the log series.
+    pub fn log_series(&self) -> &Vec<Log> {
+        &self.log_series
+    }
+
+    /// Returns the touched account set.
+    pub fn touched_accounts(&self) -> &HashSet<Address> {
+        &self.touched_accounts
+    }
+
+    /// Returns the refund balance.
+    pub fn refund_balance(&self) -> U256 {
+        self.refund_balance
+    }
+
+    /// Returns the accessed account set.
+    pub fn accessed_accounts(&self) -> &HashSet<Address> {
+        &self.accessed_accounts
+    }
+
+    /// Returns the accessed storage set.
+    pub fn accessed_storage(&self) -> &HashSet<(Address, U256)> {
+        &self.accessed_storage
+    }
 }

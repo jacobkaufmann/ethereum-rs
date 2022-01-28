cb2f4ac93b3ecbb49b123cf89f14a05c5f25635b
jacobkaufmann <jacobkaufmann18@gmail.com>
2022-01-27 23:52:41 -0700

    Add enhancements for Transaction

--- src/core/types/transaction.rs
+++ src/core/types/transaction.rs
@@ -13,3 +13,45 @@ pub struct Transaction {
     init: Option<Vec<u8>>,
     data: Option<Vec<u8>>,
 }
+
+impl Transaction {
+    pub fn nonce(&self) -> U256 {
+        self.nonce
+    }
+
+    pub fn gas_price(&self) -> U256 {
+        self.gas_price
+    }
+
+    pub fn gas_limit(&self) -> U256 {
+        self.gas_limit
+    }
+
+    pub fn to(&self) -> &Option<Address> {
+        &self.to
+    }
+
+    pub fn value(&self) -> U256 {
+        self.value
+    }
+
+    pub fn v(&self) -> U256 {
+        self.v
+    }
+
+    pub fn r(&self) -> U256 {
+        self.r
+    }
+
+    pub fn s(&self) -> U256 {
+        self.s
+    }
+
+    pub fn init(&self) -> &Option<Vec<u8>> {
+        &self.init
+    }
+
+    pub fn data(&self) -> &Option<Vec<u8>> {
+        &self.data
+    }
+}
